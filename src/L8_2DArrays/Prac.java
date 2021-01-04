e67176394c12bb820de2c42bc2a06a9e75523e35
twentyonedot <twentyonedot@gmail.com>
2021-01-04 22:30:43 +0530

    refactor

--- src/L8_2DArrays/Prac.java
+++
@@ -1,9 +0,0 @@
-package src.L8_2DArrays;
-
-public class Prac {
-    public static void main(String[] args) {
-        int[][] ar = new int[3][5];
-        System.out.println(ar[0]);
-        System.out.println(ar[1]);
-    }
-}
